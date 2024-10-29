eba0f51e7c31ba30c5b33d5389046a5c08c1880d
Gustavo Xavier Saldanha <gustavosaldxav@gmail.com>
2024-10-29 20:41:45 -0300

    Aula 03

---
+++ Aula03/03.js
@@ -0,0 +1,5 @@
+export function ola(nome) {
+  console.log(nome);
+}
+
+export const NOME_PADRAO = "anonimo";
---
+++ Aula03/04.js
@@ -0,0 +1,9 @@
+export function ola(nome) {
+  console.log(nome);
+}
+
+export const NOME_PADRAO = "anonimo";
+
+export default function soma(x, y) {
+  console.log(x + y);
+}
